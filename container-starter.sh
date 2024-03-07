6b100392e766f547ec886eeb572f0beea0bdf64d
rdias66 <dias.a@live.com>
2024-03-07 16:15:47 -0300

    server starter script done

---
+++ container-starter.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Define variables
+DOCKER_IMAGE="ubuntu:latest"
+CONTAINER_NAME="ubuntu_server"
+USER_NAME="admin"
+USER_PASS="admin"
+
+# Start the container
+docker run -dit --name "$CONTAINER_NAME" "$DOCKER_IMAGE"
+
+# Install required packages inside the container
+docker exec -it "$CONTAINER_NAME" apt-get update
+docker exec -it "$CONTAINER_NAME" apt-get install sudo
+docker exec -it "$CONTAINER_NAME" adduser --gecos "" --disabled-password "$USER_NAME"
+docker exec -it "$CONTAINER_NAME" usermod -aG sudo "$USER_NAME"
+docker exec -it "$CONTAINER_NAME" bash -c "echo 'admin:$USER_PASS' | chpasswd"
+docker exec -it "$CONTAINER_NAME" su - "$USER_NAME"
+docker exec -it "$CONTAINER_NAME" bash
+
