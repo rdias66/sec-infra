45796f302cff88915caaaa0bf28852d959abcbcd
rdias66 <dias.a@live.com>
2024-03-15 16:10:46 -0300

    dot env removed

--- main.go
+++ main.go
@@ -2,29 +2,21 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"os"
 	"sec-infra/firewall"
 	"sec-infra/proxy"
 	"sec-infra/vpn"
-
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	// Load variables from .env file
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalf("Error loading .env file: %v", err)
-	}
 
-	// Access variables from the environment
-	welcomeMessage := os.Getenv("WELCOME_MESSAGE")
+	welcomeMessage := "Welcome to the sec-infra Golang CLI, this is an app developed to optimize and facilitate the security infrastructure of a Linux Server."
 	fmt.Println(welcomeMessage)
 
 	proxy.ProxyAccessed()
 
 	firewall.FirewallAccessed()
+
+	//	firewall.SetupFirewall()
 
 	vpn.VPNAccessed()
 
